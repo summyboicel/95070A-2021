8f3ce764c0cc3c552298dae307bb9e4d0b08f820
summyboicel <57600214+summyboicel@users.noreply.github.com>
2021-11-06 13:37:46 -0700

    Removed Parallel Bar motors

--- include/autonomous.h
+++ include/autonomous.h
@@ -9,10 +9,9 @@ std::shared_ptr<ChassisController> drive =
         // Green gearset, 4 in wheel diam, 11.5 in wheel track
         .withDimensions(AbstractMotor::gearset::green, {{4_in, 11.5_in}, okapi::imev5GreenTPR})
         .build();
-std::shared_ptr<AsyncPositionController<double, double>> liftControl =
-    AsyncPosControllerBuilder().withMotor(PBPort).build();
+        
 std::shared_ptr<AsyncPositionController<double, double>> fourbar =
-    AsyncPosControllerBuilder().withMotor({FBRPort,-FBLPort}).build();
+    AsyncPosControllerBuilder().withMotor({-FBLPort}).build();
 
 //This file has all of the autonomous
 void disabledAuton(){
